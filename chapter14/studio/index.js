1510d6a0908738af0175168e309a8502739635ff
Sue <susanne_schlachta@cable.comcast.com>
2023-07-16 14:06:11 -0400

    Launch

--- chapter14/studio/index.js
+++ chapter14/studio/index.js
@@ -1,5 +1,8 @@
 let launchcode = {
-  
+  organization: 'nonprofit',
+  executiveDirector: 'Jeff',
+  percentageCoolEmployees: 100,
+  programsOffered: ['LC101', 'LaunchCode Women+', "CodeCamp"]
 }
 
 module.exports = launchcode;
--- chapter14/studio/spec/launchcode.spec.js
+++ chapter14/studio/spec/launchcode.spec.js
@@ -1,8 +1,22 @@
 // launchcode.spec.js code:
 const launchcode = require('../index.js');
 
-describe("Testing launchcode", function(){
+describe("The launchcode object", function(){
 
   // Write your unit tests here!
-  
+  it('should have a property called organization', function (){
+    expect(launchcode.organization).toEqual('nonprofit');
+  });
+  it('should have a property called edecutiveDirector', funtion(){
+    expect(launchcode.executiveDirector).toEqual('Jeff');
+  });
+  it('should have a property called percentageCoolEmployee', function(){
+    expect(launchcode.percentageCoolEmployees).toEqual(100);
+  });
+  it('should have a property called programsOffered', function(){
+    expect(launchcode.programsOffered[0]).toEqual('LC101');
+    expect(launchcode.programsOffered[1]).toEqual('Launchcode Women+');
+    expect(launchcode.programsOffered[2]).toEqual('CodeCamp');
+    expect(launchcode.programsOffered.length).toEqual(3);
+  });
 });
