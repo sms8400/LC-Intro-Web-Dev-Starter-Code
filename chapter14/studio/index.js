0c5e6dd310108143b85eeecec0b497650c65c3db
Sue <susanne_schlachta@cable.comcast.com>
2023-07-16 15:20:11 -0400

    run

--- chapter14/studio/index.js
+++ chapter14/studio/index.js
@@ -2,8 +2,20 @@ let launchcode = {
   organization: 'nonprofit',
   executiveDirector: 'Jeff',
   percentageCoolEmployees: 100,
-  programsOffered: ['LC101', 'LaunchCode Women+', "CodeCamp"]
-}
-
+  programsOffered: ['LC101', 'LaunchCode Women+', "CodeCamp"],
+  launchOutput: function(num){
+    let result ='';
+    if(num % 2===0){
+        result += 'Launch!';
+     }
+     if(num % 3===0){
+        result += 'Code!';
+     }
+     if (num % 5===0){
+        result += 'Rocks!';
+     }
+     return result += '!';
+    }
+};
 module.exports = launchcode;
 
--- chapter14/studio/spec/launchcode.spec.js
+++ chapter14/studio/spec/launchcode.spec.js
@@ -7,7 +7,7 @@ describe("The launchcode object", function(){
   it('should have a property called organization', function (){
     expect(launchcode.organization).toEqual('nonprofit');
   });
-  it('should have a property called edecutiveDirector', funtion(){
+  it('should have a property called edecutiveDirector', function (){
     expect(launchcode.executiveDirector).toEqual('Jeff');
   });
   it('should have a property called percentageCoolEmployee', function(){
@@ -18,5 +18,26 @@ describe("The launchcode object", function(){
     expect(launchcode.programsOffered[1]).toEqual('Launchcode Women+');
     expect(launchcode.programsOffered[2]).toEqual('CodeCamp');
     expect(launchcode.programsOffered.length).toEqual(3);
+  });
+
+  describe('should have a method, launchOutput, which', function(){
+    it('should return "Launch!" for numbers evenly divisiable by only 2', function(){
+      expect(launchode.launchOutput(2)).toEqual('Launch!');
+    });
+    it('should return "Code!" for numbers evenly divisible by only 3', function(){
+      expect(launch.launchOUtput(3)).toEqual('Code!');
+    });
+    it('should return "Rocks!" for numbers evenly divisible by only 5', function*(){
+      expect(launchode.launchOUtput(5)).toEqual('Rocks!');
+    });
+    it('should return "LauncCode!" for numbers evenly divisible by only 5', function(){
+      expect(launchcode.launchOutput(6)).toEqual('LaunchCode!');
+    });
+    it('should return "Code Rocks!" for numbers evenly divisible by both 3 and 5', function(){
+      expect(launchcode.launchOutput(15)).toEqual('Code Rocks!');
+    });
+    it('should return "Launch Rocks!" for numbers evenly divisible by both 2 and 5', function(){
+      expect(launchcode.launchOutput(10)).toEqual('Launch Rocks!');
+    });
   });
 });
