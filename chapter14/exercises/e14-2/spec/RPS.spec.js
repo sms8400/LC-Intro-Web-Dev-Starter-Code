40609f2f93931d23331b0f6af5a276efb98836fc
Sue <susanne_schlachta@cable.comcast.com>
2023-07-17 15:45:41 -0400

    push

--- chapter14/exercises/e14-2/spec/RPS.spec.js
+++ chapter14/exercises/e14-2/spec/RPS.spec.js
@@ -1,6 +1,10 @@
 const test = require('../RPS.js');
 
 describe("whoWon", function(){
+    it("returns 'Player 1 & 2 ties!' if P1 = rock & P2 = rock", function(){
+        let output = test.whoWon('rock','rock');
+        expect(output).toEqual("Both Players Tied!");
+     });
     it("returns 'Player 2 wins!' if P1 = rock & P2 = paper", function(){
        let output = test.whoWon('rock','paper');
        expect(output).toEqual("Player 2 wins!");
