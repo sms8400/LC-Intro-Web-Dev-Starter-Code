6fd1dd213a2f128638e796ed32e3b105e159d33c
Sue <susanne_schlachta@cable.comcast.com>
2023-07-16 22:04:55 -0400

    Ch.14Studio

--- chapter14/studio/index.js
+++ chapter14/studio/index.js
@@ -4,19 +4,32 @@ let launchcode = {
   percentageCoolEmployees: 100,
   programsOffered: ['LC101', 'LaunchCode Women+', "CodeCamp"],
   launchOutput: function(num){
+    let parts={
+        2: 'Launch',
+        3: 'Code',
+        5: 'Rocks'
+    };
     let result ='';
-    if(num % 2===0){
-        result += 'Launch!';
-     }
-     if(num % 3===0){
-        result += 'Code!';
-     }
-     if (num % 5===0){
-        if(result.length > 0){
-            result += ' ';
+    for(let modulus in parts){
+        if (num % Number(modulus) === 0){
+            if (modulus == 5 && result.length > 0){
+                result += '';
+            }
+            result += parts[modulus];
         }
-        result += 'Rocks!';
-     }
+    }
+    // if(num % 2===0){
+    //     result += 'Launch!';
+    //  }
+    //  if(num % 3===0){
+    //     result += 'Code!';
+    //  }
+    //  if (num % 5===0){
+    //     if(result.length > 0){
+    //         result += ' ';
+    //     }
+    //     result += 'Rocks!';
+    //  }
      if(result.length === 0){
         result = "Rutabagas! That doesn't work."
      }else{
