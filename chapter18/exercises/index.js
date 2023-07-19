c1b10a73b5ebb503fad93b4c2ab44a8f5caf0711
Sue <138933788+sms8400@users.noreply.github.com>
2023-07-19 11:47:33 -0400

    complete

---
+++ chapter18/exercises/index.js
@@ -0,0 +1,59 @@
+// Define your Book class here:
+class Book {
+    constructor(title, author, copyright, isbn, pages, timesCheckedOut, discarded){
+       this.title = title;
+       this.author = author;
+       this.copyright = copyright;
+       this.isbn = isbn;
+       this.pages = pages;
+       this.timesCheckedOut = timesCheckedOut;
+       this.discarded = discarded;
+    }
+ 
+    checkout(uses=1) {
+       this.timesCheckedOut += uses;
+    }
+ }
+ 
+ class Manual extends Book {
+    constructor(title, author, copyright, isbn, pages, timesCheckedOut, discarded){
+       super(title, author, copyright, isbn, pages, timesCheckedOut, discarded);
+    }
+ 
+    dispose(currentYear){
+       if (currentYear-this.copyright > 5) {
+          this.discarded = 'Yes';
+       }
+    }
+ }
+ 
+ class Novel extends Book {
+    constructor(title, author, copyright, isbn, pages, timesCheckedOut, discarded){
+       super(title, author, copyright, isbn, pages, timesCheckedOut, discarded);
+    }
+ 
+    dispose(){
+       if (this.timesCheckedOut > 100) {
+          this.discarded = 'Yes';
+       }
+    }
+ }
+
+// Define your Manual and Novel classes here:
+
+let goodRead = new Novel("Pride and Prejudice", 'Jane AudioListener', 1813, '1111111111111', 432, 32, 'No' );
+
+
+// Declare the objects for exercises 2 and 3 here:
+
+let makingTheShip = new Manual('Top Secret Shuttle Building Manual', 'Redacted', 2013, '0000000000000', 1147, 1, 'No');
+
+// Code exercises 4 & 5 here:
+function reportStatus(){
+   let stats = `${this.title} has discarded due to being ${this.copyright} the crew can throw it into empy space!`
+      return stats
+}
+
+console.log(goodRead);
+goodRead.checkout(5);
+makingTheShip.dispose(2023);
